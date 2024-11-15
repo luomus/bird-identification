ef54a2bee63ad45138e158797889fdfda43ac891
Mikko Heikkinen <hawk@biomi.org>
2024-11-16 00:10:18 +0200

    Refactor variable names, use BirdNET filename format

--- app/run_model.py
+++ app/run_model.py
@@ -27,10 +27,10 @@ output_path = "../output" # Output folder for results
 
 # Load classification model
 # TFLITE_THREADS can be as high as number of CPUs available, the rest of the parameters should not be changed
-clsf = Classifier(path_to_model='../models/model_v3_5.keras', sr=48000, clip_dur=3.0, TFLITE_THREADS = 1, offset=0, dur=0)
+audio_classifier = Classifier(path_to_model='../models/model_v3_5.keras', sr=48000, clip_dur=3.0, TFLITE_THREADS = 1, offset=0, dur=0)
 
 # Load species name list and post-processing tables for prediction calibration
-species_list = pd.read_csv("classes.csv")
+species_name_list = pd.read_csv("classes.csv")
 migration_parameters = np.load('Pred_adjustment/migration_params.npy')
 calibration_parameters = np.load('Pred_adjustment/calibration_params.npy')
 
@@ -38,47 +38,54 @@ calibration_parameters = np.load('Pred_adjustment/calibration_params.npy')
 # Todo: ignore unsupported file types
 files = os.listdir(input_path)
 
-# Create output file with header
-#with open(input_path + '_results.txt', 'a') as f:
-#    f.write("site, file, species, prediction, detection_time \n")
+
 
 n_files = len(files)
 
 # Loop each audio file in input folder
-for file_index, file in enumerate(files):
+for file_index, file_name in enumerate(files):
     try:
-        print(f"Analyzing {file} ({file_index+1}/{n_files})")
+        file_path = input_path + '/' + file_name
+        print(f"Analyzing {file_path} ({file_index + 1} of {n_files})")
+
+        # Output filename with BirdNET format
+        file_name_wo_extension = os.path.splitext(file_name)[0]
+        output_file_path = f"{output_path}/{file_name_wo_extension}.Muuttolinnut.results.csv"
+
+        # Create output file with header
+        with open(output_file_path, 'a') as output_file_writer:
+            output_file_writer.write("site, filename, species, prediction, detection_time \n")
 
         # Predict species
-        predictions, detection_times = clsf.classify(input_path + '/' + file, max_pred=False) #max_pred: only keep highest confidence detection for each species instead of saving all detections
+        species_predictions, detection_timestamps = audio_classifier.classify(file_path, max_pred=False) #max_pred: only keep highest confidence detection for each species instead of saving all detections
 
         # Calibrate prediction based on calibration parameters
-        for prediction_index in range(len(predictions)):
-            predictions[prediction_index, :] = calibrate(predictions[prediction_index, :], cal_table=calibration_parameters)
+        for detection_index in range(len(species_predictions)):
+            species_predictions[detection_index, :] = calibrate(species_predictions[detection_index, :], cal_table=calibration_parameters)
         
         # Ignore human and noise predictions by setting them to zero
         # Todo: remove?
-        predictions[:,0:2] = 0
+        species_predictions[:,0:2] = 0
 
         # Filter predictions with a threshold 
-        predictions, c, detection_times = threshold_filter(predictions, detection_times, threshold)
+        species_predictions, species_class_indices, detection_timestamps = threshold_filter(species_predictions, detection_timestamps, threshold)
 
         # Adjust prediction based on time of the year and latitude (only if record is from Finland and location and date are known)
-        predictions = adjust(predictions, c, migration_parameters, lat, lon, day_of_year) 
+        species_predictions = adjust(species_predictions, species_class_indices, migration_parameters, lat, lon, day_of_year) 
 
         # Loop through predictions
-        for prediction_index in range(len(predictions)):
-            if c[prediction_index] > 1: # ignore two first classes: noise and human
+        for detection_index in range(len(species_predictions)):
+            if species_class_indices[detection_index] > 1: # ignore two first classes: noise and human
 
                 # Save results to output file
-                output_filename = f"{output_path}/results.txt"
-                with open(output_filename, 'a') as output_file:
-                    output_file.write(
+#                output_filename = f"{output_path}/results.txt"
+                with open(output_file_path, 'a') as output_file_writer:
+                    output_file_writer.write(
                         f"{input_path}, "
-                        f"{file}, "
-                        f"{species_list['luomus_name'].iloc[c[prediction_index]]}, "
-                        f"{predictions[prediction_index]}, "
-                        f"{detection_times[prediction_index]}\n"
+                        f"{file_name}, "
+                        f"{species_name_list['luomus_name'].iloc[species_class_indices[detection_index]]}, "
+                        f"{species_predictions[detection_index]}, "
+                        f"{detection_timestamps[detection_index]}\n"
                     )
 
         # Clear memory
@@ -86,11 +93,11 @@ for file_index, file in enumerate(files):
 
     # Handle exceptions
     except: 
-        print(f"Error analyzing {file}!")
+        print(f"Error analyzing {file_name}!")
         print("Exception type:", type(e).__name__)
         print("Exception message:", str(e))
         print("Traceback:")
         traceback.print_exc()
 
 print("All files analyzed")
-print(f"Results saved to {output_filename}")
+#print(f"Results saved to {output_filename}")
