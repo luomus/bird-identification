c7b9d532adcfab4bc68f5d374fbe3afb8c653da1
aorin <meeri.rannisto@gmail.com>
2025-10-30 11:35:03 +0200

    use splash screen paintEvent instead of threading to ensure that the splash screen is shown before initializing the app

--- app/app.py
+++ app/app.py
@@ -1,25 +1,29 @@
 from PySide6.QtWidgets import QApplication, QSplashScreen
-from PySide6.QtCore import QThreadPool
+from PySide6.QtCore import Qt, QTimer, Signal
 from PySide6.QtGui import QPixmap
 
 import sys
 import resources # noqa
 
-from utils.worker import Worker
+class SplashScreen(QSplashScreen):
+    isReady = Signal()
+
+    is_ready = False
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        if not self.is_ready:
+            QTimer.singleShot(0, self.isReady)
+            self.is_ready = True
 
 app = QApplication([])
 
-splash = QSplashScreen(QPixmap(":/icons/splash.png"))
+splash = SplashScreen(QPixmap(":/icons/splash.png"))
 splash.setEnabled(False) # clicking doesn't close it
-splash.show()
 
 window = None
 
-# MainWindow import takes a long time since it imports large libraries like tensorflow. The import is done in a separate thread so that it doesn't block the splash screen
-def init_imports():
-    from widgets.main_window import MainWindow # noqa
-
-def on_ready():
+def show_main_window():
     global window
 
     from widgets.main_window import MainWindow
@@ -28,10 +32,7 @@ def on_ready():
     splash.finish(window)
     window.show()
 
-worker = Worker(init_imports)
-worker.signals.finished.connect(on_ready)
-
-thread_pool = QThreadPool()
-thread_pool.start(worker)
+splash.isReady.connect(show_main_window) # ensure that the splash screen is shown first
+splash.show()
 
 sys.exit(app.exec())
