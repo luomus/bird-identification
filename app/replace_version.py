5b135d45828597cf9c6d2306df13769fd8d33004
aorin <meeri.rannisto@gmail.com>
2025-12-01 18:43:44 +0200

    fix wrong version in windows version info

--- app/replace_version.py
+++ app/replace_version.py
@@ -2,11 +2,19 @@ from version import __version__
 
 file_paths = ["version_info.txt", "version_info_analyze.txt", "app.ifp"]
 
+version_parts = __version__.split(".")
+major = version_parts[0]
+minor = version_parts[1]
+patch = version_parts[2]
+
 for file_path in file_paths:
     with open(file_path, "r") as f:
         content = f.read()
 
     content = content.replace("{{ version }}", __version__)
+    content = content.replace("{{ major }}", major)
+    content = content.replace("{{ minor }}", minor)
+    content = content.replace("{{ patch }}", patch)
 
     with open(file_path, "w") as f:
         f.write(content)
