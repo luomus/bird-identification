de800399e72ed826e334e8eebcab25ba5990200c
aorin <meeri.rannisto@gmail.com>
2025-12-18 14:04:33 +0200

    get app version number from git tag, combine windows and macos installer workflows

--- app/replace_version.py
+++ app/replace_version.py
@@ -1,8 +1,21 @@
-from version import __version__
+from pathlib import Path
+import sys
+import re
+
+if len(sys.argv) < 2:
+    raise NameError("Missing version number argument")
+
+version = sys.argv[1]
+
+if not re.compile("^\d+\.\d+\.\d+$").match(version):
+    raise NameError("Invalid version number {}".format(version))
+
+path = Path("version.py")
+path.write_text("__version__ = \"{}\"".format(version))
 
 file_paths = ["app.spec", "version_info.txt", "version_info_analyze.txt", "app.ifp", "build_dmg.sh"]
 
-version_parts = __version__.split(".")
+version_parts = version.split(".")
 major = version_parts[0]
 minor = version_parts[1]
 patch = version_parts[2]
@@ -11,7 +24,7 @@ for file_path in file_paths:
     with open(file_path, "r") as f:
         content = f.read()
 
-    content = content.replace("{{ version }}", __version__)
+    content = content.replace("{{ version }}", version)
     content = content.replace("{{ major }}", major)
     content = content.replace("{{ minor }}", minor)
     content = content.replace("{{ patch }}", patch)
